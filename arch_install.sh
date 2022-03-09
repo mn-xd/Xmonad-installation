dbf9da137ee31e7be10ec7608a9fa8c36341413e
mn-xd <mn.xdfortnite@gmail.com>
2022-03-09 20:12:43 +0100

    adding more prompts

--- arch_install.sh
+++ arch_install.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+
 #this project is meant to be the easiest way to install arch linux
 #with your prefered desktop-enviroment
 #You can see tutorial on youtube by distrotube
 
-
+hostNameFile="/etc/hostname"
 
 timedatectl set-ntp true
 baseComponents(){
     #installing base components need to run install
-    pacman -S sudo
-    pacman -S git
+    pacman -S --noconfirm sudo
+    pacman -S --noconfirm git
 }
 diskChoser(){
     echo fdisk -l
@@ -27,7 +29,6 @@ baseKernel(){
 clock(){
     #TODO changing zone and region
     hwclock --systohc
-    pacman -S nano
     echo "Un comment your locale ex---en_US.UTF-8  UTF-8"
     sleep 5s
     #TODO add more instructions
@@ -35,38 +36,46 @@ clock(){
     locale-gen
 }
 settingUpUsers(){
-    echo "Type in your host name"
-    sleep 5s
-    nano /etc/hostname
-    #TODO check if scripts doesn't start to quicly
-    #sleep 30s
-    echo "Add this 3 lines"
-    echo "change your_host_name with hostname that you typed above"
-    echo "127.0.0.1    localhost"
-    echo "::1          localhost"
-    echo "127.0.1.1    your_host_name.localdomain    your_host_name"
-    echo "copy this and paste to file that will be in few seconds automaticly opened"
-    sleep 50s
-    nano /etc/hosts
-    echo "Set up your ROOT password"
-    passwd
-    sleep 5s
-    echo "type your username"
-    read username
-    useradd -m $username
-    echo "type password for $username"
-    passwd $username
-    usermod -aG wheel,audio,video,optical,storage $username
+    echo "Set up users? (y,n)"
+    read userSetup
+    if [$userSetup == "y"] || [$userSetup == "yes"]
+    then
+        echo "Type in your host name"
+        sleep 5s
+        read hostname
+        echo $hostname >> $hostNameFile
+        echo "Add this 3 lines"
+        echo "change your_host_name with hostname that you typed above"
+        echo "127.0.0.1    localhost"
+        echo "::1          localhost"
+        echo "127.0.1.1    your_host_name.localdomain    your_host_name"
+        echo "copy this and paste to file that will be in few seconds automaticly opened"
+        sleep 50s
+        #TODO hange it to auto
+        nano /etc/hosts
+        echo "Set up your ROOT password"
+        passwd
+        sleep 3s
+        echo "type your username"
+        read username
+        useradd -m $username
+        echo "type password for $username"
+        passwd $username
+        usermod -aG wheel,audio,video,optical,storage $username
+    elif [$userSetup == "n"] || [$userSetup == "no"]
+    then
+        baseComponents
+    fi
 }
 
 #TODO add desktop-enviroments
 
 processingFunction(){
-    baseComponents
     diskChoser
     baseKernel
     clock
     settingUpUsers
+    baseComponents
 }
 
 processingFunction
